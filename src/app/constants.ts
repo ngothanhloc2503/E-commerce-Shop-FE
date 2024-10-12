56e9d9966c6ee280b6aff77d178d1bbccd15ee01
19120563 <ngoloc2706@gmail.com>
2024-10-12 12:31:50 +0700

    docs(const): change api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://localhost:8888/api';
+// export const API_URL = 'http://localhost:8888/api';
+export const API_URL = 'https://e-commerce-shop-be-cvtf.onrender.com/api';
