7dde71798469b086fa28add0b1f38c8cefca3e0c
19120563 <ngoloc2706@gmail.com>
2025-10-05 16:14:07 +0700

    docs: change url connect to be

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8888/api';
-// export const API_URL = 'https://e-commerce-shop-be-cvtf.onrender.com/api';
+// export const API_URL = 'http://localhost:8888/api';
+export const API_URL = 'https://e-commerce-shop-be-cvtf.onrender.com/api';
